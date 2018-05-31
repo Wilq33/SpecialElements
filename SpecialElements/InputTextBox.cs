f53f7af3d60bf973c15c7d79dd58687c3dd8b6a1
Wilk Bartlomiej <wilkbartlomiej26@gmail.com>
2018-05-31 14:57:01 +0200

    1st commit
    InputTextBox

---
+++ SpecialElements/InputTextBox.cs
@@ -0,0 +1,36 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class InputTextBox
+{
+
+    static IWebDriver driver = new ChromeDriver();
+    static IWebElement textBox;
+
+    static void Main()
+    {
+        string url = "http://testing.todvachev.com/special-elements/text-input-field/";
+
+
+
+        driver.Navigate().GoToUrl(url);
+
+        textBox = driver.FindElement(By.Name("username"));
+
+        textBox.SendKeys("Test text");
+
+        Thread.Sleep(3000);
+
+        Console.WriteLine(textBox.GetAttribute("maxlength"));
+
+        Thread.Sleep(3000);
+
+        textBox.Clear();
+
+        Thread.Sleep(3000);
+
+        driver.Quit();
+    }
+}
